c1f1f658c974ab6231d09ce5bc4c8e8118d192d0
Konstantinos <kwstas654321@gmail.com>
2021-01-04 13:18:47 +0200

    Front End with Ajax

---
+++ Front End/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "http://localhost:8081/api/customer/1"
+    }).then(function(data) {
+       
+       $('#id').append(data.id);
+       $('#firstName').append(data.firstName);
+       $('#lastName').append(data.lastName);
+       $('#email').append(data.email);
+       $('#tel').append(data.tel);
+    });
+});
